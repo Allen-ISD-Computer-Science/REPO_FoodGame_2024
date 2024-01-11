ef7a935c36ecd0d095146fbf0183f7a6db65b4bd
Lettuce287 <111583609+Lettuce287@users.noreply.github.com>
2024-01-11 00:59:58 -0600

    Ported DataHandler script from studio place

---
+++ DataHandler.lua
@@ -0,0 +1,81 @@
+local DatastoreService = game:GetService("DataStoreService")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+local primaryDatastore = DatastoreService:GetDataStore("placeholderStore")
+
+function waitForReqBudget(reqType)
+	local currBudget = DatastoreService:GetRequestBudgetForRequestType(reqType)
+	while currBudget < 1 do
+		currBudget = DatastoreService:GetRequestBudgetForRequestType(reqType)
+		task.wait(5)
+	end
+end
+
+function EnsureProtectedCall(plr: Player, func, self, reqType, ...)
+	local success, result
+
+	repeat
+		if reqType then
+			waitForReqBudget(reqType)
+		end
+		success, result = pcall(func, self, ...)
+		if not success then
+			warn("Error: "..result)
+			if string.find(result, "501") or string.find(result, "504") then return end
+		end
+	until (success) or (plr == nil) or (plr.Name and not Players:FindFirstChild(plr.Name))
+	return success, result
+end
+
+function loadData(plr: Player)
+	if not Players:FindFirstChild(plr.Name) then return end
+	local success, playerData = EnsureProtectedCall(plr, primaryDatastore.GetAsync, primaryDatastore, Enum.DataStoreRequestType.GetAsync, plr.UserId)
+	if not success then return end
+	--TBD: DataService, SettingService, PlayerData class modules
+end
+
+local function saveData(plr: Player, noDelay)
+	-- local playerData = PlayerModule.GetPlayer(plr.UserId) [METHOD INCOMPLETE]
+	if not playerData then return end
+	local dataToSave = {
+		--populate with stuff from DataService, SettingService when completed
+	}
+	EnsureProtectedCall(nil, primaryDatastore.UpdateAsync, primaryDatastore, (not noDelay and Enum.DataStoreRequestType.UpdateAsync), plr.UserId, function()
+		return dataToSave
+	end)
+end
+
+local function closeServer()
+	if RunService:IsStudio() then task.wait(2) else
+		local complete = Instance.new("BindableEvent")
+		local allPlayers = Players:GetPlayers()
+		local remainingPlayers = #allPlayers
+
+		for _, player in ipairs(allPlayers) do
+			coroutine.wrap(function()
+				saveData(player, true)
+				remainingPlayers -= 1
+				if remainingPlayers == 0 then complete:Fire() end
+			end)()
+		end
+		complete.Event:Wait()
+	end
+end
+
+for _, player in ipairs(Players:GetPlayers()) do
+	coroutine.wrap(loadData)(player)
+end
+
+Players.PlayerAdded:Connect(loadData)
+Players.PlayerRemoving:Connect(function(plr)
+	saveData(plr)
+	PlayerModule.RemovePlayer(plr.UserId)
+end)
+
+game:BindToClose(closeServer)
+
+while task.wait(300) do
+	for _, player in ipairs(Players:GetPlayers()) do
+		coroutine.wrap(saveData)(player)
+	end
+end
